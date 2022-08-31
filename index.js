ec0274b4600a16b2ba4326a7ac08c1b13da68211
Chaitanya Gupta <mailchaim007@gmail.com>
2022-08-31 21:59:08 +0530

    front and back end completed

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const shortid = require('shortid')
+const path = require('path');
+const link = require('./models/link')
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URI,{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
+
+const PORT = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:false}))
+
+app.get("/", (req,res)=>{
+    res.render('index.ejs')
+})
+
+let mainId = "";
+let mainUrl = "";
+app.get("/link", async (req,res)=>{
+    res.render('link.ejs', {link : mainId, url: mainUrl})
+    mainId = "";
+    mainUrl = "";
+})
+
+app.get('/checkBacklink', (req,res)=>{
+    const backlink = req.query.backlink;
+    link.findOne({short : backlink})
+    .then(doc => {
+        if(doc){
+            res.json({
+                status: true,
+            })
+        }else{
+            res.json({
+                status: false,
+            })
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
+app.post("/link", async(req,res)=>{
+    mainUrl = req.body.fullurl
+    backlink = req.body.backlink
+    await link.create({
+        full : mainUrl,
+        short: backlink,
+    });
+    mainId = backlink;
+    res.redirect("/link")
+})
+
+app.get('/:shortUrl', async(req,res)=>{
+    const shortUrl = await link.findOne({short: req.params.shortUrl})
+    if(shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.save();
+
+    res.redirect(shortUrl.full)
+})
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ models/link.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+
+const link = new mongoose.Schema({
+    fulllink: {
+        type: String,
+        required: true
+    },
+    shortendedlink: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Link', shortUrls)
